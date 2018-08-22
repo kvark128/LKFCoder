ff624709cc2ec2e68ed9c1ac454b4a1766fb830c
kvark128 <kvark128@yandex.ru>
2018-08-23 04:51:41 +0800

    Code refactoring

--- LKFCoder.go
+++ LKFCoder.go
@@ -16,7 +16,7 @@ import (
 const (
 	blockSize = 128 // The block size in words. Every word of 32 bit
 	delta     = 0x9e3779b9
-	version   = "0.3" // Program version
+	version   = "0.4" // Program version
 )
 
 // The 128-bit key for encrypting/decrypting lkf files. It is divided into 4 parts of 32 bit each.
@@ -33,88 +33,98 @@ func calcKey(leftWord, rightWord, r, k uint32) uint32 {
 	return n1 ^ n2
 }
 
-// decode function decrypts the lkf-file and writes the result to the source file, then changes the extension to .mp3
+// decoder function decrypts the lkf-file and writes the result to the source file, then changes the extension to .mp3
 // Decoding occurs by blocks from the beginning of the file. If the end of file is less than block size, it remains as it is.
-func decode(srcFile *os.File, wg *sync.WaitGroup, semaphoreCH <-chan struct{}, errorsCH chan<- error) {
+func decoder(files <-chan *os.File, wg *sync.WaitGroup, errors chan<- error) {
 	var block [blockSize]uint32
 	defer wg.Done()
-	defer func() {
+	for srcFile := range files {
+		for binary.Read(srcFile, binary.LittleEndian, &block) == nil {
+			for r := uint32(3); r != 0; r-- {
+				for k := blockSize - 1; k >= 0; k-- {
+					block[k] -= calcKey(block[(k-1)&(blockSize-1)], block[(k+1)&(blockSize-1)], r*delta, uint32(k))
+				}
+			}
+
+			// Moving on 1 block back, for record the decoded block
+			if _, err := srcFile.Seek(blockSize*-4, 1); err != nil {
+				errors <- err
+				break
+			}
+
+			if err := binary.Write(srcFile, binary.LittleEndian, &block); err != nil {
+				errors <- err
+				break
+			}
+		}
+
 		path := srcFile.Name()
 		srcFile.Close()
 		os.Rename(path, path[:len(path)-3]+"mp3")
-		<-semaphoreCH
-	}()
-
-	for binary.Read(srcFile, binary.LittleEndian, &block) == nil {
-		for r := uint32(3); r != 0; r-- {
-			for k := blockSize - 1; k >= 0; k-- {
-				block[k] -= calcKey(block[(k-1)&(blockSize-1)], block[(k+1)&(blockSize-1)], r*delta, uint32(k))
-			}
-		}
-
-		// Moving on 1 block back, for record the decoded block
-		if _, err := srcFile.Seek(blockSize*-4, 1); err != nil {
-			errorsCH <- err
-			break
-		}
-
-		if err := binary.Write(srcFile, binary.LittleEndian, &block); err != nil {
-			errorsCH <- err
-			break
-		}
 	}
 }
 
-// encode function encrypts the mp3-file and writes the result to the source file, then changes the extension to .lkf
+// encoder function encrypts the mp3-file and writes the result to the source file, then changes the extension to .lkf
 // Encoding occurs by blocks from the beginning of the file. If the end of file is less than block size, it remains as it is.
-func encode(srcFile *os.File, wg *sync.WaitGroup, semaphoreCH <-chan struct{}, errorsCH chan<- error) {
+func encoder(files <-chan *os.File, wg *sync.WaitGroup, errors chan<- error) {
 	var block [blockSize]uint32
 	defer wg.Done()
-	defer func() {
+	for srcFile := range files {
+		for binary.Read(srcFile, binary.LittleEndian, &block) == nil {
+			for r := uint32(1); r != 4; r++ {
+				for k := 0; k < blockSize; k++ {
+					block[k] += calcKey(block[(k-1)&(blockSize-1)], block[(k+1)&(blockSize-1)], r*delta, uint32(k))
+				}
+			}
+
+			// Moving on 1 block back, for record the encoded block
+			if _, err := srcFile.Seek(blockSize*-4, 1); err != nil {
+				errors <- err
+				break
+			}
+
+			if err := binary.Write(srcFile, binary.LittleEndian, &block); err != nil {
+				errors <- err
+				break
+			}
+		}
+
 		path := srcFile.Name()
 		srcFile.Close()
 		os.Rename(path, path[:len(path)-3]+"lkf")
-		<-semaphoreCH
-	}()
-
-	for binary.Read(srcFile, binary.LittleEndian, &block) == nil {
-		for r := uint32(1); r != 4; r++ {
-			for k := 0; k < blockSize; k++ {
-				block[k] += calcKey(block[(k-1)&(blockSize-1)], block[(k+1)&(blockSize-1)], r*delta, uint32(k))
-			}
-		}
-
-		// Moving on 1 block back, for record the encoded block
-		if _, err := srcFile.Seek(blockSize*-4, 1); err != nil {
-			errorsCH <- err
-			break
-		}
-
-		if err := binary.Write(srcFile, binary.LittleEndian, &block); err != nil {
-			errorsCH <- err
-			break
-		}
 	}
 }
 
-func main() {
-	args := []string{2: "./"}
-	copy(args, os.Args)
-	var srcExt string
-	var counterFiles int
-
-	var action func(*os.File, *sync.WaitGroup, <-chan struct{}, chan<- error)
+func workerCreator(worker func(<-chan *os.File, *sync.WaitGroup, chan<- error), files chan *os.File, errors chan error) {
 	wg := new(sync.WaitGroup)
-	errorsCH := make(chan error)
-	semaphoreCH := make(chan struct{}, runtime.NumCPU())
+
+	for n := runtime.NumCPU(); n > 0; n-- {
+		wg.Add(1)
+		go worker(files, wg, errors)
+	}
+
+	go func() {
+		wg.Wait()
+		close(errors)
+	}()
+}
+
+func main() {
+	var args = []string{2: "./"}
+	copy(args, os.Args)
+
+	var counterFiles int
+	var srcExt string
+	files := make(chan *os.File)
+	errors := make(chan error)
 
 	log.SetFlags(0)
 	switch args[1] {
 	case "decode":
-		action = decode
+		workerCreator(decoder, files, errors)
 		srcExt = ".lkf"
 	case "encode":
-		action = encode
+		workerCreator(encoder, files, errors)
 		srcExt = ".mp3"
 	case "version":
 		log.Println("LKFCoder version", version)
@@ -124,24 +134,17 @@ func main() {
 	}
 
 	walker := func(path string, info os.FileInfo, err error) error {
-		if err != nil {
+		if info.IsDir() || strings.ToLower(filepath.Ext(path)) != srcExt || err != nil {
 			return err
-		}
-
-		if info.IsDir() || strings.ToLower(filepath.Ext(path)) != srcExt {
-			return nil
 		}
 
 		srcFile, err := os.OpenFile(path, os.O_RDWR, 0644)
 		if err != nil {
-			log.Println(path, err)
-			return nil
+			return err
 		}
 
-		semaphoreCH <- struct{}{}
-		wg.Add(1)
 		counterFiles++
-		go action(srcFile, wg, semaphoreCH, errorsCH)
+		files <- srcFile
 		return nil
 	}
 
@@ -150,13 +153,9 @@ func main() {
 	if err := filepath.Walk(args[2], walker); err != nil {
 		log.Println(err)
 	}
+	close(files)
 
-	go func() {
-		wg.Wait()
-		close(errorsCH)
-	}()
-
-	for err := range errorsCH {
+	for err := range errors {
 		log.Println(err)
 	}
 	finishTime := time.Now()
