37958b7d10326e708fdf5d4a08248272fb6f9749
Kvark <kvark128@yandex.ru>
2022-07-02 00:26:39 +0800

    Logging is now thread safe

--- LKFCoder.go
+++ LKFCoder.go
@@ -2,9 +2,9 @@
 package main
 
 import (
-	"fmt"
 	"io"
 	"io/fs"
+	"log"
 	"os"
 	"path/filepath"
 	"runtime"
@@ -15,7 +15,9 @@ import (
 	"github.com/kvark128/lkf"
 )
 
-func FileCryptor(path string, cryptor func(*lkf.Cryptor, []byte) int) error {
+type CryptorFunc func(*lkf.Cryptor, []byte) int
+
+func FileCryptor(path string, cryptor CryptorFunc) error {
 	f, err := os.OpenFile(path, os.O_RDWR, 0)
 	if err != nil {
 		return err
@@ -58,23 +60,24 @@ func FileCryptor(path string, cryptor func(*lkf.Cryptor, []byte) int) error {
 	return f.Close()
 }
 
-func worker(pathCH <-chan string, wg *sync.WaitGroup, targetExt string, cryptor func(*lkf.Cryptor, []byte) int) {
+func worker(pathCH <-chan string, wg *sync.WaitGroup, logger *log.Logger, targetExt string, cryptor CryptorFunc) {
 	defer wg.Done()
 	for path := range pathCH {
 		targetPath := path[:len(path)-4] + targetExt
 		tmpPath := targetPath + ".tmp"
 		if err := os.Rename(path, tmpPath); err != nil {
-			fmt.Printf("worker: %v\n", err)
+			logger.Printf("worker: %v\n", err)
 			continue
 		}
 
 		if err := FileCryptor(tmpPath, cryptor); err != nil {
-			fmt.Printf("worker: %v\n", err)
+			logger.Printf("worker: %v\n", err)
 			continue
 		}
 
 		if err := os.Rename(tmpPath, targetPath); err != nil {
-			fmt.Printf("worker: %v\n", err)
+			logger.Printf("worker: %v\n", err)
+			continue
 		}
 	}
 }
@@ -83,10 +86,11 @@ func main() {
 	var action string
 	var fileCounter int
 	var srcExt, targetExt string
-	var cryptor func(*lkf.Cryptor, []byte) int
+	var cryptor CryptorFunc
 	wg := new(sync.WaitGroup)
 	pathCH := make(chan string)
 	workingDir := "./"
+	logger := log.New(os.Stdout, "", 0)
 
 	if len(os.Args) >= 2 {
 		action = os.Args[1]
@@ -106,13 +110,12 @@ func main() {
 		srcExt = ".mp3"
 		targetExt = ".lkf"
 	default:
-		fmt.Printf("Unsupported action specified\n")
-		os.Exit(1)
+		logger.Fatalf("Unsupported action specified\n")
 	}
 
 	for n := runtime.NumCPU(); n > 0; n-- {
 		wg.Add(1)
-		go worker(pathCH, wg, targetExt, cryptor)
+		go worker(pathCH, wg, logger, targetExt, cryptor)
 	}
 
 	walker := func(path string, d fs.DirEntry, err error) error {
@@ -126,11 +129,11 @@ func main() {
 
 	start := time.Now()
 	if err := filepath.WalkDir(workingDir, walker); err != nil {
-		fmt.Printf("Filewalker: %v\n", err)
+		logger.Printf("Filewalker: %v\n", err)
 	}
 
 	close(pathCH)
 	wg.Wait()
 	finish := time.Since(start)
-	fmt.Printf("Processed %d *%s files in %v\n", fileCounter, srcExt, finish)
+	logger.Printf("Processed %d *%s files in %v\n", fileCounter, srcExt, finish)
 }
