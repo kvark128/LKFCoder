5478c7c3d57ba9a2828f3f6e7661428e929062b3
Kvark <kvark128@yandex.ru>
2022-07-01 21:56:25 +0800

    More reliable error checking

--- LKFCoder.go
+++ LKFCoder.go
@@ -16,40 +16,46 @@ import (
 )
 
 func FileCryptor(path string, cryptor func(*lkf.Cryptor, []byte) int) error {
-	f, err := os.OpenFile(path, os.O_RDWR, 0644)
+	f, err := os.OpenFile(path, os.O_RDWR, 0)
 	if err != nil {
 		return err
 	}
-	defer f.Close()
 
 	buf := make([]byte, lkf.BlockSize*1024) // 512 Kb
 	c := new(lkf.Cryptor)
+	var off int64
 
 	for err == nil {
-		var n int
-		n, err = io.ReadFull(f, buf)
-		if n < lkf.BlockSize {
-			break
-		}
+		err = func() error {
+			n, err := io.ReadFull(f, buf)
+			if err != nil && err != io.ErrUnexpectedEOF {
+				// Fatal error when reading from file
+				// Processing must be aborted immediately
+				return err
+			}
 
-		// Encrypt or decrypt the read data
-		cryptor(c, buf[:n])
-
-		// Moving on n bytes back, for record the decrypted/encrypted data
-		if _, err := f.Seek(-int64(n), io.SeekCurrent); err != nil {
+			// Encrypt or decrypt the read data
+			// Processed data should be written back to the file instead of the previously read ones
+			if np := cryptor(c, buf[:n]); np != 0 {
+				if _, err := f.WriteAt(buf[:np], off); err != nil {
+					// Fatal error when writing to file
+					// Processing must be aborted immediately
+					return err
+				}
+				off += int64(np)
+			}
 			return err
-		}
-
-		if _, err := f.Write(buf[:n]); err != nil {
-			return err
-		}
+		}()
 	}
 
-	if err == io.ErrUnexpectedEOF {
-		// Just an end of the file. Skip this error
-		err = nil
+	if err != io.ErrUnexpectedEOF {
+		// Fatal error occurred while processing the file. Close the file and return this error
+		f.Close()
+		return err
 	}
-	return err
+
+	// Just the end of the file with no errors. Closing it
+	return f.Close()
 }
 
 func worker(pathCH <-chan string, wg *sync.WaitGroup, targetExt string, cryptor func(*lkf.Cryptor, []byte) int) {
